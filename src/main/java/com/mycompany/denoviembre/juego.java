9272d03e3c99a99be0edd5d65352ac4036bda3a6
lorena_olay <olaylorena2002@gmail.com>
2021-11-03 14:19:09 +0100

    controlado numeros aleatorios y su suma y condicionales para elegir ganador y empate

--- src/main/java/com/mycompany/denoviembre/juego.java
+++ src/main/java/com/mycompany/denoviembre/juego.java
@@ -4,6 +4,9 @@
  */
 package com.mycompany.denoviembre;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author lorena
@@ -14,7 +17,24 @@ public class juego {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int jugador1, jugador2, rondas, suma;
+        Scanner entradateclado = new Scanner(System.in);
+        Random numAleatorio = new Random();
         // TODO code application logic here
+        
+        System.out.println("número aleatorio del jugador 1");
+        jugador1 = numAleatorio.nextInt(6);
+        System.out.println("número aleatorio del jugador 2");
+        jugador2 = numAleatorio.nextInt(6);
+        suma = jugador1 + jugador2;
+        if(suma < 7 ){
+            System.out.println("Jugador 1 ganas");
+        }else if (suma > 7){
+            System.out.println("Jugador 2 ganas");
+        }else if ( suma == 7){
+            System.out.println("No gana ninguno");
+        }
+
     }
-    
+
 }
