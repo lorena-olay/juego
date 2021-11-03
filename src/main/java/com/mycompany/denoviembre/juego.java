7f3964ca75db8bfc137ddab583c7b8ae7c52bbe3
lorena_olay <olaylorena2002@gmail.com>
2021-11-03 14:24:17 +0100

    hecho bucle de rondas para victorias de los 2 usuarios

--- src/main/java/com/mycompany/denoviembre/juego.java
+++ src/main/java/com/mycompany/denoviembre/juego.java
@@ -17,23 +17,31 @@ public class juego {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int jugador1, jugador2, rondas, suma;
+        int jugador1, jugador2, rondas, suma, rondas1, rondas2;
+        rondas1 = 0;
+        rondas2 = 0;
         Scanner entradateclado = new Scanner(System.in);
         Random numAleatorio = new Random();
         // TODO code application logic here
-        
+        do{
         System.out.println("número aleatorio del jugador 1");
         jugador1 = numAleatorio.nextInt(6);
         System.out.println("número aleatorio del jugador 2");
         jugador2 = numAleatorio.nextInt(6);
         suma = jugador1 + jugador2;
-        if(suma < 7 ){
+        if (suma < 7) {
             System.out.println("Jugador 1 ganas");
-        }else if (suma > 7){
+        } else if (suma > 7) {
             System.out.println("Jugador 2 ganas");
-        }else if ( suma == 7){
+        } else if (suma == 7) {
             System.out.println("No gana ninguno");
         }
+        if (suma < 7) {
+            rondas1++;
+        } else if (suma > 7){
+            rondas2++;
+        }
+        }while(!(rondas1 == 3 || rondas2 == 3));
 
     }
 
